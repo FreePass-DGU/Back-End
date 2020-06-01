077874db63afa7cf2053e51b305b36cd6568a264
Ju-Yeon Park <pjuyeon25@gmail.com>
2020-06-02 03:30:09 +0900

    add show specific course info function

--- course/course.php
+++ course/course.php
@@ -1,4 +1,4 @@
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Type" "application/json; charset=utf-8" />
 <?php 
 
     //로그인 이후 저장되는 세션 형식에 따라 변경할 부분
@@ -24,14 +24,13 @@
         exit();
     }
 
-
+    $Return_FinalArray = array();
     //전체 코스 목록 출력
     if (!isset($_GET['id'])) { 
 
         $sql = "select * from course";
         $result = mysqli_query($con, $sql);
     
-        $Return_FinalArray = array();
         if ($result->num_rows > 0) {
 
             while( $row = mysqli_fetch_array($result) ) {
@@ -49,7 +48,6 @@
      //유저가 고른 조건에 따른 코스 목록 출력
     } elseif (isset($_GET['id']) and $_GET['id'] == 0) {
     
-            
             //가장 최근 유저의 필터링 조건 기록
             $sql = "select hashtags from history where h_id = (
                 select max(h_id) from history where u_id = ".$user_id.")";
@@ -61,7 +59,6 @@
             $courses = mysqli_query($con, $sql);
 
             //필터링 조건에 맞는 코스 고르기
-            $Return_FinalArray = array();
             if ($courses->num_rows > 0){
                 while($row = mysqli_fetch_array($courses)){
                     foreach($hashtags as $hashtag){
@@ -83,7 +80,135 @@
 
     // 특정 코스에 대한 정보 출력
     } elseif (isset($_GET['id']) and $_GET['id'] != 0){
-    
+        
+        //해당 코스 받아오기
+        $sql = "select * from course where cs_id = ".$_GET['id'];
+        $course =  mysqli_query($con, $sql);
+        $row = mysqli_fetch_array($course);
+        $course = array (
+            'id' => $row["cs_id"],
+            'name' => $row["cs_name"],
+            'purpose' => $row["cs_purpose"],
+            'spt' => $row['Spt_id'],
+            'ept' => $row['Ept_id']
+        );
+        
+        
+        //코스 시작 지점, 끝 지점 받아오기
+        $area_ids = array();
+        $sql = "select * from point where pt_id = '".$course['spt']."'";
+        $result =  mysqli_query($con, $sql);
+        $row = mysqli_fetch_array($result);
+        $course['spt'] = $row["pt_name"];
+        array_push($area_ids, $row["area_id"]);
+
+        $sql = "select * from point where pt_id = '".$course['ept']."'";
+        $row =  mysqli_query($con, $sql);
+        $row = mysqli_fetch_array($row);
+        $course['ept'] = $row["pt_name"];
+        array_push($area_ids, $row["area_id"]);
+
+        if($area_ids[1] == "A" or $area_ids[1] == "B"){
+            array_push($area_ids, "X");
+        }else{
+            array_push($area_ids, "Y");
+        }
+
+        if($area_ids[0] == $area_ids[1]){
+            unset($area_ids[1]);
+            $area_ids = array_values($area_ids);
+        }
+
+        //해시태그 가져오기
+        $purposes = explode(';',$course['purpose']);
+
+        //해당 디비 다 들고오기
+        $foods = array();
+        $places = array();
+        $cafes = array();
+        
+        foreach($area_ids as $area_id){
+            $sql = "select * from food where area_id = \"".$area_id."\"";
+            $result =  mysqli_query($con, $sql);
+
+            if ($result->num_rows > 0) {
+                while( $row = mysqli_fetch_array($result) ) {
+                    $flag = 0;
+                    foreach($purposes as $purpose){
+                        if(preg_match('/'.$purpose.'/', $row["f_purpose"]))
+                            $flag = 1;
+                    }
+                    if(!$flag) continue;
+
+                    $my_array = array (
+                        'id' => $row["f_id"],
+                        'name' => $row["f_name"],
+                        'purpose' => $row["f_purpose"],
+                        'info' => $row["f_info"],
+                        'url' => $row["f_url"]
+                    );
+                    array_push($foods,$my_array);
+                }
+            }else {
+                //echo '{}';
+            }
+
+
+            $sql = "select * from cafe where area_id = \"".$area_id."\"";
+            $result =  mysqli_query($con, $sql);
+
+            if ($result->num_rows > 0) {
+                while( $row = mysqli_fetch_array($result) ) {
+                    $flag = 0;
+                    foreach($purposes as $purpose){
+                        if(preg_match('/'.$purpose.'/', $row["c_purpose"]))
+                            $flag = 1;
+                    }
+                    if(!$flag) continue;
+
+                    $my_array = array (
+                        'id' => $row["c_id"],
+                        'name' => $row["c_name"],
+                        'purpose' => $row["c_purpose"],
+                        'info' => $row["c_info"],
+                        'url' => $row["c_url"]
+                    );
+                    array_push($cafes,$my_array);
+                }
+            }else {
+                //echo '{}';
+            }
+
+            $sql = "select * from place where area_id = \"".$area_id."\"";
+            $result =  mysqli_query($con, $sql);
+
+            if ($result->num_rows > 0) {
+                while( $row = mysqli_fetch_array($result) ) {
+                    $flag = 0;
+                    foreach($purposes as $purpose){
+                        if(preg_match('/'.$purpose.'/', $row["p_purpose"]))
+                            $flag = 1;
+                    }
+                    if(!$flag) continue;
+                    $my_array = array (
+                        'id' => $row["p_id"],
+                        'name' => $row["p_name"],
+                        'purpose' => $row["p_purpose"],
+                        'info' => $row["p_info"],
+                        'url' => $row["p_url"]
+                    );
+                    array_push($places,$my_array);
+
+                }
+            }else {
+                //echo '{}';
+            }
+        }
+
+        $course['food'] = $foods;
+        $course['cafe'] = $cafes;
+        $course['place'] = $places;
+        $Return_FinalArray = $course;
 
     } else {
         echo '{"status": 400, "messages": "wrong access"}';
@@ -91,13 +216,11 @@
     }   
 
 
-    
     $arr = array(
         'status' => '200',            
         'message' => '코스 출력 완료'
     );
     $arr['data'] = $Return_FinalArray;  
-    
     
     // encode php array to json string
     $RESULT = json_encode($arr, JSON_UNESCAPED_UNICODE);     
